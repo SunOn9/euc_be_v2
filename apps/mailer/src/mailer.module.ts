6a31cdb3f83a9a66073fcede621c6933298fd8e6
Woka Tech <lehophattai@gmail.com>
2024-01-10 16:35:51 +0700

    init

--- apps/mailer/src/mailer.module.ts
+++ apps/mailer/src/mailer.module.ts
@@ -1,9 +1,57 @@
 import { Module } from '@nestjs/common';
 import { MailerController } from './mailer.controller';
 import { MailerService } from './mailer.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import * as dotenv from 'dotenv';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+
+dotenv.config();
+const cwd = process.cwd();
+// const synchronizeDatabaseStructure = process.env.APP_ENV == 'dev';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(cwd, 'uploads'),
+      renderPath: '/uploads/*',
+      serveRoot: '/uploads',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(cwd, 'data'),
+      renderPath: '/data/*',
+      serveRoot: '/data',
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        ttl: configService.get('REDIS_CACHE_TTL'),
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+        isGlobal: true,
+      }),
+      isGlobal: true,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [MailerController],
   providers: [MailerService],
 })
---
+++ scripts/setup_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+SCRIPT=$(pwd $0)
+
+# docker pull mysql:8.1.0
+# docker pull redis:7.2.0
+# docker pull elasticsearch:8.9.0
+
+# docker run -d --name euc-mysql -p 3307:3306 \
+#     --env MYSQL_ROOT_PASSWORD=190501 \
+#     --env TZ=Asia/Ho_Chi_Minh \
+#     -v /Volumes/ImageDisk/docker/mysql:/var/lib/mysql \
+#     mysql:8.1.0
+
+# docker run -d --name euc-redis -p 6379:6379 \
+#     --volume=/Volumes/ImageDisk/docker/redis:/data \
+#     redis:7.2.0
+
+# docker run -d --name euc-elasticsearch -p 9200:9200 -p 9300:9300 \
+#     --env ELASTIC_PASSWORD=190501 \
+#     -e discovery.type="single-node" -e ES_JAVA_OPTS="-Xms256m -Xmx512m" \
+#     elasticsearch:8.9.0
